48b07f75367ac9018a146c61dc09f5123c05f4cf
Pierre PAVY <p.alex.pavy@gmail.com>
2017-05-13 10:52:27 +0200

    file input managed as an argument for YoutubeWownloader.py

--- YoutubeMusicDownloader.py
+++ YoutubeMusicDownloader.py
@@ -5,15 +5,29 @@
 #Create a txt file with URLs to music you want to download from Youtube
 #One URL per line
 
+#-- How to --
+#run the script with your file as an argument as below
+#./YoutubeMusicDownloader "path/to/your/file" 
+
 import pafy
 import os
+import sys
 
-#Set here the path to your file
-file = open("tracks.txt", "r")
+fileList = []
 
-for line in file :
-	target = pafy.new(line)
-	audio = target.audiostreams
-	audio[0].download()
+for arg in sys.argv:
+	print(arg)
+	fileList.append(arg)
+
+i = 1
+
+while i < len(fileList) :
+	file = open(fileList[i], "r")
+	for line in file :
+		print(line)
+		target = pafy.new(line)
+		audio = target.audiostreams
+		audio[0].download()
+		i +=1
 
 file.close()
